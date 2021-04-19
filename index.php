31d122b2adf3f88f12b258af6e62ae5370424902
nishachoudhary111 <55096692+nishachoudhary111@users.noreply.github.com>
2021-04-19 09:43:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--created by Nisha Kumari-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Spark Bank</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">    
+</head>
+<body>
+    <header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="images/logo.png" alt="logo" style="width: 150px;">
+        </div>
+        <ul class="nav-area">
+        <li><a href="#">Home</a></li>
+        <li><a href="viewcustomers.php">View Customer</a></li>
+        
+        </ul>
+    </div>
+    <div class="welcome-text">
+        <h1>
+            Welcome to <span><h5>Spark Bank<h5></span></h1>
+            
+    </div>
+</header>
+
+</body>
+</html>
